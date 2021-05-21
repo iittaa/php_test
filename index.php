39ea1a9b97f7f7eb1dbbe27eca5570d5abd744a7
板本　耕輔 <kosukeitamoto@itamotokosukenoMacBook-Pro.local>
2021-05-21 14:43:40 +0900

    php学習

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+こちらはHTMLです
 <?php
+echo "こちらはPHPです";
+echo "<br>";
+// 変数指定
+$test = "こんにちは";
 
+// 定数指定（一般的には全て大文字）
+const MAX = "テスト";
+echo MAX;
 
-$test = 123;
-$test_2 = 456;
-echo $test;
+echo "<br>";
+echo "こちらは${test}です";
+
 var_dump($test);
-exit;
-echo $test_2;
+
+// 配列一行
+$array = ["あああ", 2, 3];
+
+$array_2 = [
+  ["赤", "青", "黄色"],
+  ["紫", "緑", "黒"]
+];
+
+echo "<br>";
+echo $array[0];
+
+echo "<br>";
+echo $array_2[0][1];
+
+echo "<br>";
+echo "<pre>";
+var_dump($array);
+echo "<pre>";
+
+echo "<pre>";
+var_dump($array_2);
+echo "<pre>";
+
+
+
+
+
+
 
 ?>
+
+
+</body>
