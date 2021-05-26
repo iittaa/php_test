55b25dc0f193c3dd8b44819853964a7ebfd4b912
板本　耕輔 <kosuke.723723@gmail.com>
2021-05-26 14:49:50 +0900

    ファイルの書き方

--- mainte/test.php
+++ mainte/test.php
@@ -1,13 +1,33 @@
 <?php
 // パスワードを記録した場所
-echo __FILE__;
-echo "<br>";
+// echo __FILE__;
+// echo "<br>";
 
 // /Applications/MAMP/htdocs/php_test/mainte/test.php
-echo(password_hash("password", PASSWORD_BCRYPT));
+// echo(password_hash("password", PASSWORD_BCRYPT));
 
 
+$file = ".contact.dat";
 
+// ファイルを丸ごと読み込み
+$contents = file_get_contents($file);
+
+// echo $contents;
+
+// $add = "テストです。" . PHP_EOL;
+
+// ファイルに書き込み（上書き）
+// file_put_contents($file, "$add", FILE_APPEND);
+
+// 配列 file, 区切る, 
+$allData = file($file);
+
+foreach ($allData as $lineData) {
+  $lines = explode(",", $lineData);
+  echo $lines[0] . "<br>";
+  echo $lines[1] . "<br>";
+  echo $lines[2] . "<br>";
+}
 
 
 ?>
